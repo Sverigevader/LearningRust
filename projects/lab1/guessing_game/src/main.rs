5b83807ad5b7a59c8c0473ec5ef9bd18204e94e0
sverigevader <robert.temiz@consid.se>
2021-11-14 23:53:02 +0100

    lagt till rng och .expect är fortfarande otydligt...

--- projects/lab1/guessing_game/src/main.rs
+++ projects/lab1/guessing_game/src/main.rs
@@ -1,15 +1,24 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Welcome to the Guessing Game. 
     I'm thinking of a number, your job is to guess it using as few guesses as possible");
     println!("Please input your guess");
 
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("The secret number is: {}", secret_number);
+
+    println!("Please input your guess!");
+    
     let mut guess = String::new();
 
     io::stdin()
     .read_line(&mut guess)
-    .expect("Failed to read line!");
+    // Följande rad betyder essentiellt att om det inte fungerar; 
+    // Prefixa felmeddelande i "Err" med argumentet (strängen) som är given. 
+    // Känns som att formuleringen blir lite galen va?
+    .expect("Failed to read line!"); 
 
     println!("You guessed: {}", guess);
 }
